b3b4f54246d5fe15255b4eb8c7a0dc7c7f96ae09
zhulaoda10 <1716829025@qq.com>
2020-03-20 19:55:42 +0800

    Update DeleteTable.java
    优化

--- spring-hive-hbase/src/main/java/com/neo/hbase/DeleteTable.java
+++ spring-hive-hbase/src/main/java/com/neo/hbase/DeleteTable.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
 
 public class DeleteTable {
@@ -13,11 +17,15 @@ public static void main(String[] args) throws IOException {
 		Configuration config = HBaseConfiguration.create();
  
 		// 建立 admin
-		HBaseAdmin admin = new HBaseAdmin(config);
- 
+	        //此种方式已过时不建议使用
+		//HBaseAdmin admin = new HBaseAdmin(config);
+		Connection connection = ConnectionFactory.createConnection(config); 
+		Admin admin = connection.getAdmin();
+		//转化为表名
+                TableName name = TableName.valueOf("mebr");//具体的表明
 		// 先 disable 表，再delete
-		admin.disableTable("people");
-		admin.deleteTable("people");
+		admin.disableTable(name);
+		admin.deleteTable(name);
 	}
 
 }
